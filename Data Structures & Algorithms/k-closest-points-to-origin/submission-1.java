90199ba1242e421a04cf264a49a90a9134837fe0
Fares <168811758+fareskhelifi@users.noreply.github.com>
2026-08-31 22:54:39 +0100

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Map.Entry<int[], Double>> pq = new PriorityQueue<>(
+            Comparator.comparingDouble(Map.Entry::getValue)
+        );
+
+        Map<int[], Double> map = new HashMap<>(); 
+
+        for (int[] point : points) {
+            int x1 = point[0], x2 = point[1];
+            Double distance = Math.sqrt(Math.pow(x1, 2) + Math.pow(x2, 2));
+            map.put(point, distance);
+        }
+        
+        pq.addAll(map.entrySet());
+
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.poll().getKey();
+        }
+
+        return result;
+
+    }
+}
